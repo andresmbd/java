de1f584418ca626dce00ae5fef985afac5da27fa
Andres <anmabadi@outlook.com>
2026-08-03 20:28:56 -0500

    creado arquitectura

---
+++ src/main/java/com/mycompany/arquitecturacapas/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arquitecturacapas;
+
+/**
+ *
+ * @author cohorte_5
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
